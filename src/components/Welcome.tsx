7014b0b66a9a44991747a40078b82f9bb26e3ae7
emmanuellsk8 <emmanuel50354@gmail.com>
2025-07-11 10:01:12 -0500

    feat: Welcome component fixed

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -10,18 +10,20 @@ function Welcome() {
                     <SquareCheckBig strokeWidth={2.8} size={37} />
                     SMART ORDER</p>
             </div>
-            <div className="flex w-full bg-gray-100 justify-center items-center">
-                <div className="w-full flex flex-col items-center ">
-                    <div className="max-w-[500px] rounded-2xl overflow-hidden bg-white object-cover aspect-square flex flex-col justify-between items-center mb-16">
+
+            <div className="flex w-full bg-gray-100 justify-center items-center ">
+                <div className="w-full flex flex-col items-center max-[1100px]:mb-16">
+                    <div className="max-w-[500px] rounded-2xl overflow-hidden bg-white object-cover aspect-square flex flex-col justify-between items-center mt-16">
                         <img src="https://images.ctfassets.net/pdf29us7flmy/7sWTrrGJjnAtn64qmCcoHd/4a45fc456549dba844a64b3c0b5582bb/cuanto-gana-mesero-estados-unidos.png?w=720&q=100&fm=jpg" alt="Meseros"
                         />
-                        <p className="text-4xl font-semibold text-nowrap">Panel de Meseros</p>
+                        <p className="text-4xl font-semibold text-nowrap ">Panel de Meseros</p>
                         <div className="w-full px-8">
                         <button className="mb-7 hover:scale-105 duration-300 ease-in-out text-white bg-orange-500/90 py-2.5 rounded-lg w-full cursor-pointer" onClick={() => navigate("/wait-staff")}>Acceder ahora</button>
                         </div>
                     </div>
                 </div>
             </div>
+
             <div className="relative w-full justify-center hidden max-[1100px]:flex">
             <div className="absolute pointer-events-none -mt-5 ">
                 <p className="flex gap-2 text-4xl font-extrabold">
@@ -29,10 +31,11 @@ function Welcome() {
                     SMART ORDER</p>
             </div>
             </div>
+
             <div className="flex w-full bg-gray-300 justify-center items-center ">
-                <div className="w-full flex flex-col items-center ">
+                <div className="w-full flex flex-col items-center max-[1100px]:mb-16">
                     <div className="max-w-[500px] rounded-2xl overflow-hidden bg-white object-cover aspect-square flex flex-col justify-between items-center mt-16">
-                        <img src="https://www.nestleprofessional-latam.com/sites/default/files/styles/np_article_big/public/2023-04/mejor-chef-del-mundo-exito_0.jpg?itok=r2TucJZJ" alt="Meseros"
+                        <img src="https://www.nestleprofessional-latam.com/sites/default/files/styles/np_article_big/public/2023-04/mejor-chef-del-mundo-exito_0.jpg?itok=r2TucJZJ" alt="Chef"
                         />
                         <p className="text-4xl font-semibold text-nowrap ">Panel de Chefs</p>
                         <div className="w-full px-8">
